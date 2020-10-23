c5d8bb2b9877d1b4d93a11dde2ab588291984849
Joseph Steel <62897806+SteelMasterJ@users.noreply.github.com>
2020-10-23 13:53:40 -0400

    Update Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -14,7 +14,7 @@ const Profile = props => (
                     <ul class="list-group list-group-flush card-body">
                         <li class="list-group-item">Book Value: {props.bookValue}</li>
                         <li class="list-group-item">Dividend Per Share: {props.DividendPerShare}</li>
-                        <li class="list-group-item">Earnings Per Share (Diluted): {props.eps}</li>
+                        <li class="list-group-item">Earnings Per Share (Diluted, Trailing Tweleve Months): {props.eps}</li>
                         <li class="list-group-item">Graham Number: {Math.sqrt(22.5 * props.bookValue * props.eps)}</li>
                     </ul>
                 </div>

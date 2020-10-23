d683f974669bf312dd57745ca154c74e35c9e933
Joseph Steel <62897806+SteelMasterJ@users.noreply.github.com>
2020-10-23 13:41:56 -0400

    Graham Number Functional!

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -9,13 +9,13 @@ const Profile = props => (
                     <div class="card-body">
                         <h5 class="card-title">{props.ticker}</h5>
                         <p class="card-text">{props.businessType}</p>
-                        <a rel="noopener noreferrer" href={props.websiteUrl} class="btn btn-primary" target="_blank">Company Website</a>
+                        <a rel="noopener noreferrer" href={"https://finviz.com/quote.ashx?t=" + props.ticker} class="btn btn-primary" target="_blank">Company Website</a>
                     </div>
                     <ul class="list-group list-group-flush card-body">
                         <li class="list-group-item">Book Value: {props.bookValue}</li>
                         <li class="list-group-item">Dividend Per Share: {props.DividendPerShare}</li>
                         <li class="list-group-item">Earnings Per Share (Diluted): {props.eps}</li>
-                        <li class="list-group-item">Graham Number: </li>
+                        <li class="list-group-item">Graham Number: {Math.sqrt(22.5 * props.bookValue * props.eps)}</li>
                     </ul>
                 </div>
             </div>

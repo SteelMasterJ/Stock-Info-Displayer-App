0f7e7e1624f752819d830c4dcce7c74ac33d53df
Joseph Steel <62897806+SteelMasterJ@users.noreply.github.com>
2020-10-31 11:38:23 -0400

    Update Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -14,7 +14,7 @@ const Profile = props => (
                     <a rel="noopener noreferrer" href={"https://finviz.com/quote.ashx?t=" + props.ticker} className="btn btn-primary" target="_blank">More Info</a>
                 </div>
                 <ul className="list-group list-group-flush card-body">
-                    <li className={(props.bookValue > 0) ? "list-group-item" : "list-group-item text-danger"}>Book Value: {props.bookValue}</li>
+                    <li className={(props.bookValue > 0) ? "list-group-item" : "list-group-item text-danger"}>Book Value Per Share: {props.bookValue}</li>
                     <li className="list-group-item">Dividend Per Share: {props.DividendPerShare}</li>
                     <li className={(props.eps > 0) ? "list-group-item" : "list-group-item text-danger"}>Earnings Per Share (Diluted, Trailing Tweleve Months): {props.eps}</li>
                     <li className="list-group-item">Graham Number: {isNaN(Math.sqrt(22.5 * props.bookValue * props.eps)) ? "Don't buy this stock, it's fundamentals are flawed and/or some of its financial data is missing" : Math.sqrt(22.5 * props.bookValue * props.eps)}</li>

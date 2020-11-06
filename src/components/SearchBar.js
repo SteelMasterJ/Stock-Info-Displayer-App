778c969fa90888efe35f05e218505311c4bc7efb
Joseph Steel <62897806+SteelMasterJ@users.noreply.github.com>
2020-11-06 14:25:02 -0500

    working on layout

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -40,32 +40,44 @@ const SearchForm = (props) => {
   
   return (
     <nav className="navbar navbar-expand-sm navbar-light bg-light">
-      <div className="navbar-brand"><a className="nav-link text-dark" href="/">Graham Number Stock Displayer</a></div>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
-        <li className="nav-item">
-          <a className="nav-link" href="/about">About</a>
-        </li>
-        </ul>
-      </div>
-      <form className="search-form form-inline ml-auto navbar-text" onSubmit={handleSubmit} >
-        {(props.error === "Too many requests, please wait 1 minute" || "Stock Data Not Found, Try Again") ? <div className="text-danger">{props.error}</div> : <div></div> }
-        <input 
-              className="form-control mr-sm-2"
-              type="search" 
-              onChange={handleValueChange}
-              name="search"
-              placeholder="Search"
-              aria-label="Search"
-              required
-              />
-        <button type="submit" id="submit" className="search-button btn btn-outline-info my-2 my-sm-0">
-          Search
+      <div className="container-fluid">
+        <div className="row align-items-center d-flex">
+          <div className="col">
+            <div className="navbar-brand"><a className="nav-link text-dark" href="/">Graham Number Stock Displayer</a></div>
+          </div>
+        </div>
+        <div className="w-100"></div>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
-      </form>  
+        <div className="row align-items-center d-flex">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="/about">About</a>
+            </li>
+            </ul>
+          </div>
+        </div>
+        <div className="w-100"></div>
+        <div className="row align-items-center d-flex">
+          <form className="search-form form-inline ml-auto navbar-text" onSubmit={handleSubmit} >
+            {(props.error === "Too many requests, please wait 1 minute" || "Stock Data Not Found, Try Again") ? <div className="text-danger">{props.error}</div> : <div></div> }
+            <input 
+                  className="form-control mr-sm-2"
+                  type="search" 
+                  onChange={handleValueChange}
+                  name="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                  required
+                  />
+            <button type="submit" id="submit" className="search-button btn btn-outline-info my-2 my-sm-0">
+              Search
+            </button>
+          </form>
+        </div>
+      </div> 
     </nav>  
   );
 }

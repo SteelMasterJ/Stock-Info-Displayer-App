fe626eb3e99fcd7d73a160fd7279d2e4e607456e
Joseph Steel <62897806+SteelMasterJ@users.noreply.github.com>
2020-11-12 16:14:11 -0500

    build folder made

---
+++ build/precache-manifest.29df66c61dc14442e5b360c70c05c400.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f9596c1502a1f5953795a181678141c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "381016589a107fe88154",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "374e0801d35a87d6e8a3",
+    "url": "/static/js/2.3820398c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3820398c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "381016589a107fe88154",
+    "url": "/static/js/main.6b25333e.chunk.js"
+  },
+  {
+    "revision": "4a1d6c0a84253d435b21",
+    "url": "/static/js/runtime-main.292b67c6.js"
+  }
+]);
